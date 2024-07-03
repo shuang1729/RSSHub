f70b9b29e243641f7faf552f98a0e03c2e31bd2d
Shuang <coding@samwushuang.com>
2024-07-03 16:15:39 +0800

    Update package.json and registry.ts for Heroku build

--- lib/registry.ts
+++ lib/registry.ts
@@ -23,85 +23,79 @@ let namespaces: Record<
     }
 > = {};
 
-const loadNamespaces = async () => {
-    switch (process.env.NODE_ENV) {
-        case 'test':
-        case 'production':
-            // @ts-expect-error
-            namespaces = await import('../assets/build/routes.json');
-            break;
-        default:
-            modules = directoryImport({
-                targetDirectoryPath: path.join(__dirname, './routes'),
-                importPattern: /\.ts$/,
-            }) as typeof modules;
-    }
+switch (process.env.NODE_ENV) {
+    case 'test':
+    case 'production':
+        // @ts-expect-error
+        namespaces = await import('../assets/build/routes.json');
+        break;
+    default:
+        modules = directoryImport({
+            targetDirectoryPath: path.join(__dirname, './routes'),
+            importPattern: /\.ts$/,
+        }) as typeof modules;
+}
 
-    if (Object.keys(modules).length) {
-        for (const module in modules) {
-            const content = modules[module] as
-                | {
-                      route: Route;
-                  }
-                | {
-                      namespace: Namespace;
-                  };
-            const namespace = module.split(/[/\\]/)[1];
-            if ('namespace' in content) {
-                namespaces[namespace] = Object.assign(
-                    {
-                        routes: {},
-                    },
-                    namespaces[namespace],
-                    content.namespace
-                );
-            } else if ('route' in content) {
-                if (!namespaces[namespace]) {
-                    namespaces[namespace] = {
-                        name: namespace,
-                        routes: {},
-                    };
-                }
-                if (Array.isArray(content.route.path)) {
-                    for (const path of content.route.path) {
-                        namespaces[namespace].routes[path] = {
-                            ...content.route,
-                            location: module.split(/[/\\]/).slice(2).join('/'),
-                        };
-                    }
-                } else {
-                    namespaces[namespace].routes[content.route.path] = {
+if (Object.keys(modules).length) {
+    for (const module in modules) {
+        const content = modules[module] as
+            | {
+                  route: Route;
+              }
+            | {
+                  namespace: Namespace;
+              };
+        const namespace = module.split(/[/\\]/)[1];
+        if ('namespace' in content) {
+            namespaces[namespace] = Object.assign(
+                {
+                    routes: {},
+                },
+                namespaces[namespace],
+                content.namespace
+            );
+        } else if ('route' in content) {
+            if (!namespaces[namespace]) {
+                namespaces[namespace] = {
+                    name: namespace,
+                    routes: {},
+                };
+            }
+            if (Array.isArray(content.route.path)) {
+                for (const path of content.route.path) {
+                    namespaces[namespace].routes[path] = {
                         ...content.route,
                         location: module.split(/[/\\]/).slice(2).join('/'),
                     };
                 }
+            } else {
+                namespaces[namespace].routes[content.route.path] = {
+                    ...content.route,
+                    location: module.split(/[/\\]/).slice(2).join('/'),
+                };
             }
         }
     }
-    return namespaces;
-};
+}
 
-export const namespacesPromise = loadNamespaces();
+export { namespaces };
 
 const app = new Hono();
-
-namespacesPromise.then((namespaces) => {
-    for (const namespace in namespaces) {
-        const subApp = app.basePath(`/${namespace}`);
-        for (const path in namespaces[namespace].routes) {
-            const wrappedHandler: Handler = async (ctx) => {
-                if (!ctx.get('data')) {
-                    if (typeof namespaces[namespace].routes[path].handler !== 'function') {
-                        const { route } = await import(`./routes/${namespace}/${namespaces[namespace].routes[path].location}`);
-                        namespaces[namespace].routes[path].handler = route.handler;
-                    }
-                    ctx.set('data', await namespaces[namespace].routes[path].handler(ctx));
+for (const namespace in namespaces) {
+    const subApp = app.basePath(`/${namespace}`);
+    for (const path in namespaces[namespace].routes) {
+        const wrappedHandler: Handler = async (ctx) => {
+            if (!ctx.get('data')) {
+                if (typeof namespaces[namespace].routes[path].handler !== 'function') {
+                    const { route } = await import(`./routes/${namespace}/${namespaces[namespace].routes[path].location}`);
+                    namespaces[namespace].routes[path].handler = route.handler;
                 }
-            };
-            subApp.get(path, wrappedHandler);
-        }
+                ctx.set('data', await namespaces[namespace].routes[path].handler(ctx));
+            }
+        };
+        subApp.get(path, wrappedHandler);
     }
-});
+}
 
 app.get('/', index);
 app.get('/robots.txt', robotstxt);
